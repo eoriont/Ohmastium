7dc242599342601d86ee5902ff8e250b816dac0b
Ari Katz <arikatz.d@gmail.com>
2018-12-01 17:32:47 -0800

    Old go iteration of Ohmastium

--- test/test.go
+++ test/test.go
@@ -6,63 +6,52 @@ import (
 )
 
 type Block struct {
-	name    string
-	texture string
-	item    *BlockItem
+	Name    string
+	Texture string
+	Item    *BlockItem
 }
 
 type BlockItem struct {
-	name    string
-	texture string
-	block   *Block
+	Name    string
+	Texture string
+	Block   *Block
 }
 
 type Element struct {
-	name    string
-	texture string
-	etrons  int
-	atrons  int
+	Name    string
+	Texture string
+	Etrons  int
+	Atrons  int
 }
 
 type Bucket struct {
-	name     string
-	texture  string
-	isFilled bool
+	Name     string
+	Texture  string
+	Kind     string
+	IsFilled bool
+}
+
+func (b Bucket) IsBucketFilled() bool {
+	return b.IsFilled
+}
+func (b Bucket) BucketType() string {
+	return b.Kind
 }
 
 type Armour struct {
-	name     string
-	texture  string
+	Name     string
+	Texture  string
 	kind     string
-	strength string
-}
-
-//Methods that initialize Element as an item type
-func (e Element) ItemName() string {
-	return e.name
-}
-func (e Element) ItemTexture() string {
-	return e.texture
-}
-
-//Methods that initialize BlockItem as an item type
-func (i BlockItem) ItemName() string {
-	return i.name
-}
-func (i BlockItem) ItemTexture() string {
-	return i.texture
+	Strength string
 }
 
 //Custom Element Methods
 func (e Element) GetParticles() (int, int) {
-	return e.etrons, e.atrons
+	return e.Etrons, e.Atrons
 }
 
 //Item method schematic (NOTE: if struct does not has these methods then it is not an item)
-type Item interface {
-	ItemName() string
-	ItemTexture() string
-}
+type Item interface{}
 
 func isElement(i Item) bool {
 	return reflect.TypeOf(i) == reflect.TypeOf(Element{})
@@ -76,31 +65,41 @@ type StorageSlot struct {
 func main() {
 	var (
 		DIRT_ITEM = BlockItem{
-			name:    "Dirt",
-			texture: "Brown",
+			Name:    "Dirt",
+			Texture: "Brown",
 		}
 		DIRT_BLOCK = Block{
-			name:    "Dirt",
-			texture: "Brown",
+			Name:    "Dirt",
+			Texture: "Brown",
 		}
 	)
 	BindPlacable(&DIRT_ITEM, &DIRT_BLOCK)
 	var (
 		OHMASTIUM = Element{
-			name:    "Ohmastium",
-			texture: "Bluish Grey",
-			etrons:  1,
-			atrons:  1,
+			Name:    "Ohmastium",
+			Texture: "Bluish Grey",
+			Etrons:  1,
+			Atrons:  1,
 		}
 	)
 	fmt.Println(isElement(DIRT_ITEM)) //false
 	fmt.Println(isElement(OHMASTIUM)) //true
+
+	var (
+		WATER_BUCKET = Bucket{
+			Name:    "Water Bucket",
+			Texture: "Grey",
+			Kind:    "water",
+		}
+	)
+	myBucket := StorageSlot{WATER_BUCKET, 1}
+	fmt.Println(isElement(myBucket.item))
 
 	testStack := StorageSlot{OHMASTIUM, 60}
 	fmt.Println(testStack)
 }
 
 func BindPlacable(i *BlockItem, b *Block) {
-	i.block = b
-	b.item = i
+	i.Block = b
+	b.Item = i
 }
